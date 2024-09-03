f9423fd94a15467d2e000fa747199f147b25e8c4
jcesa <j.cesar.costa2013@outlook.com>
2024-09-03 15:59:47 -0300

    "Adiciona classe de testes AutoApplicationTest para verificar o contexto da aplicação Spring Boot"

---
+++ src/test/java/br/com/biblioteca/autor/AutorApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.biblioteca.autor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorApplicationTests {
+
+}
