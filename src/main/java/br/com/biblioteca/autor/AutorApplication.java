fc2d0f32a709c59224f61c4e54ff1cd9786b4d99
jcesa <j.cesar.costa2013@outlook.com>
2024-09-03 15:55:46 -0300

    "Adiciona classe AutoApplication como ponto de entrada para a aplicação Spring Boot"

---
+++ src/main/java/br/com/biblioteca/autor/AutorApplication.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.autor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorApplication.class, args);
+	}
+}
