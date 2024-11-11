4f347fc367a30f35eb10adcdf7502b464b0867fb
jcesa <j.cesar.costa2013@outlook.com>
2024-11-11 19:53:08 -0300

    só um teste tirei a barra do livro

--- src/main/java/br/com/biblioteca/autor/controller/LivroController.java
+++ src/main/java/br/com/biblioteca/autor/controller/LivroController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/livros")
+@RequestMapping("livros")
 public class LivroController {
 
     private final LivroService livroService;
